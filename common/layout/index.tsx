31fa3b2e30cf296f07f1263497a45eaf03af295c
praise <praiseadeoti99@gmail.com>
2022-10-15 21:00:47 +0100

    update

--- common/layout/index.tsx
+++ common/layout/index.tsx
@@ -11,24 +11,52 @@ const Wrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: flex-start;
+  justify-content: space-between;
+  position: relative;
 `;
-const Container = styled.div`
-  width: 100%;
+const NavContainer = styled.div`
+  width: 90px;
+  position: fixed;
+`;
+const SideContainer = styled.div`
+  width: -webkit-fill-available;
   display: flex;
-  /* height: 100vh; */
   flex-direction: column;
   justify-content: space-between;
+  padding-left: 90px;
+  /* position: relative; */
+  .header {
+    position: fixed;
+    top: 0;
+    width: calc(100% - 90px);
+    height: 73px;
+    z-index: 10;
+  }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: calc(100% - 90px);
+  }
+  .content {
+    padding-top: 73px;
+  }
 `;
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <Wrapper>
-      <Sidebar />
-      <Container>
-        <Header />
-        {children}
-        <Footer />
-      </Container>
+      <NavContainer>
+        <Sidebar />
+      </NavContainer>
+      <SideContainer>
+        <div className="header">
+          <Header />
+        </div>
+        <div className="content">{children}</div>
+        <div className="footer">
+          <Footer />
+        </div>
+      </SideContainer>
     </Wrapper>
   );
 }
